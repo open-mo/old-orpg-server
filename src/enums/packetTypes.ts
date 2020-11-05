c17d65760b54f56c5ac05249677588f5cc7f6772
coelhucas <lucascoelhodacosta@gmail.com>
2020-11-05 19:58:19 -0300

    chore: Add both Client and Server PacketTypes

---
+++ src/enums/packetTypes.ts
@@ -0,0 +1,13 @@
+export enum ServerPackets {
+    LoginOk,
+    LoginInvalid,
+    AccountCreated,
+    UsernameAlreadyExists,
+    Size
+}
+
+export enum ClientPackets {
+    NewAccount,
+    Login,
+    Size
+}
