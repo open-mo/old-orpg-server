b6764223f95214f07e0955c8f13db85b42de6da5
coelhucas <lucascoelhodacosta@gmail.com>
2020-11-05 20:01:10 -0300

    chore: update current server logic state

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Hello, world!');
+import 'module-alias/register';
+import WebSocket from 'ws';
+import { initMessageHandler } from "handlers/dataHandler"
+import { initializeDB } from "./database";
+import {Dictionary, Packet} from "types/index";
+
+const PORT: number = 8060;
+const wss = new WebSocket.Server({ port: PORT });
+
+let messageHandler: Dictionary<any>;
+
+const initialize = async () => {
+    initializeDB();
+    messageHandler = initMessageHandler();
+}
+
+wss.on('connection', (ws: WebSocket) => {
+    ws.on('message', (message: string) => {
+        const { pkt, data }: Packet = JSON.parse(message);
+        messageHandler[pkt]({ client: ws, ...data });
+    });
+
+    ws.on('close', () => {
+
+    });
+});
+
+initialize();
+console.log(`Server listening at *:${PORT}`);
